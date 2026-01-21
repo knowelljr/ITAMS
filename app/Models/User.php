d00e3a50ff2f7cd10837ebeb1f9af3a812668c20
knowelljr <132828042+knowelljr@users.noreply.github.com>
2026-01-21 16:52:11 +0300

    Create User model with authentication methods

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,41 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\Builder;
 
 class User extends Model
 {
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password', 'role'];
 
-    public static function createUser($name, $email, $password)
+    public static function create(array $data)
     {
-        return self::create([
-            'name' => $name,
-            'email' => $email,
-            'password' => Hash::make($password),
-        ]);
+        $data['password'] = Hash::make($data['password']);
+        return self::query()->create($data);
+    }
+
+    public static function findById($id)
+    {
+        return self::query()->find($id);
     }
 
     public static function findByEmail($email)
     {
-        return self::where('email', $email)->first();
+        return self::query()->where('email', $email)->first();
     }
 
-    public function verifyPassword($password)
+    public static function verifyPassword($user, $password)
     {
-        return Hash::check($password, $this->password);
+        return Hash::check($password, $user->password);
     }
 
-    public static function updateUser($id, $data)
+    public static function updateRole($id, $role)
     {
-        $user = self::find($id);
-        if (isset($data['password'])) {
-            $data['password'] = Hash::make($data['password']);
+        $user = self::findById($id);
+        if ($user) {
+            $user->role = $role;
+            $user->save();
+            return $user;
         }
-        return $user->update($data);
-    }
-
-    public static function deleteUser($id)
-    {
-        return self::destroy($id);
+        return null;
     }
 }
